c57f2b462c100e7e23445ef7718f91942d97057c
Andrew Stephens <andrewmediaprod@gmail.com>
2018-02-18 22:06:21 -0800

    Auto-reconnect socket when disconnected

--- public_html/main.js
+++ public_html/main.js
@@ -1,11 +1,20 @@
 var SCREEN_WIDTH = 64;
 var SCREEN_HEIGHT = 32;
+var SOCKET_ADDR = "ws://localhost:8000/ws";
 
 var screenPixels = null;
-var socket;
+var socket = null;
+var refreshInterval = null;
 
 document.addEventListener('DOMContentLoaded', function() {
-   
+
+	drawScreen();
+	initSocket();
+
+}, false);
+
+var drawScreen = function() {
+
 	var screen = document.getElementById('screen');
 	screenPixels = [];
 	for (var j = 0; j < SCREEN_HEIGHT; j++) {
@@ -18,27 +27,41 @@ document.addEventListener('DOMContentLoaded', function() {
 		}
 		screen.appendChild(document.createElement('br'));
 	}
+	screen.style.width = (SCREEN_WIDTH * 20) + "px";
 
-	socket = new WebSocket("ws://localhost:8000/ws");
+}
+
+var initSocket = function() {
+	socket = new WebSocket(SOCKET_ADDR);
 	socket.onopen = function() {
 	    console.log("Socket is open");
+	    if (refreshInterval != null) {
+	    	console.log("Socket reconnected.")
+	    	clearTimeout(refreshInterval);
+	    	refreshInterval = null;
+	    }
 	};
 	socket.onmessage = function (e) {
 	    var display = JSON.parse(e.data);
 	    for (var j = 0; j < display.Grid.length; j++) {
 	    	for (var i = 0; i < display.Grid[j].length; i++) {
 	    		var pixel = display.Grid[j][i];
-	    		console.log(j, i, pixel);
-	    		console.log(screenPixels[j][i]);
+	    		// console.log(j, i, pixel);
+	    		// console.log(screenPixels[j][i]);
 	    		var rgb = "rgb(" + pixel.R + "," + pixel.G + "," + pixel.B + ")";
-	    		console.log(rgb);
+	    		// console.log(rgb);
 	    		screenPixels[j][i].style.background = rgb;
 	    	}
 	    }
 
 	}
 	socket.onclose = function () {
-	    console.log("Socket closed");
+		console.log("Socket closed");
+	    if (refreshInterval != null) {
+	    	return;
+	    }
+	    refreshInterval = window.setInterval(function() {
+	    	initSocket();
+	    }, 5000)
 	}
-
-}, false);
+}
