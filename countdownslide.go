5cd3dda79298cc8b2106e7a1e1f2efbeb046f5e6
Andrew Stephens <andrewmediaprod@gmail.com>
2020-06-10 20:31:34 -0400

    Add life event countdown slide

---
+++ countdownslide.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+    "image"
+    "image/color"
+    "math"
+    "time"
+    "fmt"
+)
+
+type CountdownSlide struct {
+
+}
+
+func NewCountdownSlide() *CountdownSlide {
+    sl := new(CountdownSlide)
+    return sl
+}
+
+func (this *CountdownSlide) Initialize() {
+    
+}
+
+func (this *CountdownSlide) Terminate() {
+
+}
+
+func (this *CountdownSlide) StartDraw(d Display) {
+    DrawOnce(d, this.Draw)
+}
+
+func (this *CountdownSlide) StopDraw() {
+
+}
+
+func (this *CountdownSlide) IsEnabled() bool {
+    return true
+}
+
+func (this *CountdownSlide) Draw(img *image.RGBA) {
+    haircutDate := time.Date(2020, time.June, 18, 0, 0, 0, 0, time.Local)
+    this.DrawCountdownLine(img, haircutDate, "HAIRCUTS", color.RGBA{0,255,255,255}, 3)
+
+    jobDate := time.Date(2020, time.July, 17, 0, 0, 0, 0, time.Local)
+    this.DrawCountdownLine(img, jobDate, "LYUBA'S LAST DAY", color.RGBA{255,0,255,255}, 13)
+    
+    moveDate := time.Date(2020, time.August, 15, 0, 0, 0, 0, time.Local)
+    this.DrawCountdownLine(img, moveDate, "NEW APARTMENT", color.RGBA{0,255,0,255}, 23)
+}
+
+func (this *CountdownSlide) DrawCountdownLine(img *image.RGBA, d time.Time, event string, c color.RGBA, y int) {
+    numberColor := color.RGBA{255,255,255,255}
+
+    WriteString(img, fmt.Sprintf("%d", this.DaysUntil(d)), numberColor, ALIGN_RIGHT, 20, y)
+    WriteString(img, event, c, ALIGN_LEFT, 26, y)
+}
+
+func (this *CountdownSlide) DaysUntil(d time.Time) int {
+    diff := time.Until(d).Hours() / 24.0
+    if diff < 0 {
+        return 0
+    }
+    return int(math.Ceil(diff))
+}
