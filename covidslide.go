3bdf72f9155ac32634220ceae247c45d660d6732
Andrew Stephens <andrewmediaprod@gmail.com>
2020-06-29 12:33:47 -0400

    Reformat Covid slide to show more data with less crowding

--- covidslide.go
+++ covidslide.go
@@ -13,10 +13,16 @@ import (
     "time"
 )
 
+// How many days into the past to fetch and graph
+// This should be one more than the number of days to graph
+// since the graph looks at diffs between days.
+var HISTORICAL_COVID_DAYS = 29
+
 type CovidSlide struct {
     // Store historically retrieved data
     UsCases map[civil.Date]int
     MaCases map[civil.Date]int
+    AzCases map[civil.Date]int
 }
 
 type DatedCases struct {
@@ -28,15 +34,19 @@ func NewCovidSlide() *CovidSlide {
     this := new(CovidSlide)
     this.UsCases = make(map[civil.Date]int)
     this.MaCases = make(map[civil.Date]int)
+    this.AzCases = make(map[civil.Date]int)
     return this
 }
 
 // We only re-query on initialization since this data only updates once a day
 func (this *CovidSlide) Initialize() {
     // Get data up to 15 days in the past
-    for i := -1; i >= -15; i-- {
-        d := civil.DateOf(time.Now().AddDate(0, 0, i))
-        if _, dOk := this.UsCases[d]; !dOk {
+    for i := 1; i <= HISTORICAL_COVID_DAYS; i++ {
+        d := civil.DateOf(time.Now().AddDate(0, 0, -i))
+        // Check if fetch was successful based on data presence
+        _, dOk := this.UsCases[d]
+        // Refresh if data is 1 or 2 days old, since it might not be stable
+        if !dOk || i < 3 {
             this.QueryForDate(d)
         }
     }
@@ -62,10 +72,11 @@ func (this *CovidSlide) IsEnabled() bool {
 func (this *CovidSlide) Draw(img *image.RGBA) {
     red := color.RGBA{255, 0, 0, 255}
 
-    WriteString(img, "COVID-19 CASES", red, ALIGN_CENTER, 63, 2)
+    WriteString(img, "COVID-19 CASES", red, ALIGN_CENTER, 63, 0)
 
-    this.DrawForLocation(img, 13, "United States", this.UsCases)
-    this.DrawForLocation(img, 22, "Massachusetts", this.MaCases)
+    this.DrawForLocation(img, 8, "US", this.UsCases)
+    this.DrawForLocation(img, 16, "Mass", this.MaCases)
+    this.DrawForLocation(img, 24, "Ariz", this.AzCases)
 }
 
 func (this *CovidSlide) DrawForLocation(img *image.RGBA, y int, label string, cases map[civil.Date]int) {
@@ -77,15 +88,15 @@ func (this *CovidSlide) DrawForLocation(img *image.RGBA, y int, label string, ca
 
     WriteString(img, label, white, ALIGN_LEFT, 1, y)
     if n1, ok := cases[d1]; ok {
-        WriteString(img, this.Format(n1), yellow, ALIGN_RIGHT, 88, y)
+        WriteString(img, this.Format(n1), yellow, ALIGN_RIGHT, 63, y)
         if n2, ok := cases[d2]; ok {
-            WriteString(img, this.Diff(n1, n2), yellow, ALIGN_RIGHT, 110, y)
+            WriteString(img, this.Diff(n1, n2), yellow, ALIGN_RIGHT, 94, y)
         }
     }
 
     var diffValues []float64
     var diffMax float64
-    for i := -15; i < -1; i++ {
+    for i := -HISTORICAL_COVID_DAYS; i < -1; i++ {
         dA := civil.DateOf(time.Now().AddDate(0, 0, i))
         dB := civil.DateOf(time.Now().AddDate(0, 0, i+1))
         nA, okA := cases[dA]
@@ -99,13 +110,15 @@ func (this *CovidSlide) DrawForLocation(img *image.RGBA, y int, label string, ca
         }
     }
 
-    DrawNormalizedGraph(img, 113, y+6, 7, 0, diffMax, yellow, diffValues)
+    DrawNormalizedGraph(img, 128-HISTORICAL_COVID_DAYS, y+6, 7, 0, diffMax, yellow, diffValues)
 }
 
 // Limit to only displaying four glyphs max
 func (this *CovidSlide) Format(n int) string {
     // Switch on number of digits in the number
     switch int(math.Log10(float64(n))) + 1 {
+    case 4:
+        return fmt.Sprintf("%.1fk", float64(n)/float64(1000))
     case 5, 6:
         return fmt.Sprintf("%.0fk", float64(n)/float64(1000))
     case 7:
@@ -128,7 +141,7 @@ func (this *CovidSlide) Diff(n1, n2 int) string {
 }
 
 // Can't use HttpHelper since the data doesn't change frequently
-// and we need to do queries to draw the slide.
+// and we need to do many queries to draw the slide.
 func (this *CovidSlide) QueryForDate(d civil.Date) {
     url := fmt.Sprintf("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/%02d-%02d-%04d.csv",
         d.Month, d.Day, d.Year)
@@ -152,8 +165,9 @@ func (this *CovidSlide) QueryForDate(d civil.Date) {
         return
     }
 
-    maSum := 0
     usSum := 0
+    maSum := 0
+    azSum := 0
     for i, row := range rows {
         // Skip header row
         if i == 0 {
@@ -174,8 +188,12 @@ func (this *CovidSlide) QueryForDate(d civil.Date) {
         if row[2] == "Massachusetts" {
             maSum += n
         }
+        if row[2] == "Arizona" {
+            azSum += n
+        }
     }
 
     this.UsCases[d] = usSum
     this.MaCases[d] = maSum
+    this.AzCases[d] = azSum
 }
