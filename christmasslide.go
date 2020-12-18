ca7b73d70c22762e746d38132d9ff0349181de7c
Andrew Stephens <andrewmediaprod@gmail.com>
2020-12-18 14:40:19 -0500

    Add animated tree to Christmas slide

--- christmasslide.go
+++ christmasslide.go
@@ -5,12 +5,41 @@ import (
     "image"
     "image/color"
     "math"
+    "math/rand"
     "time"
 )
 
+var TreeDef = [][]int{
+    {10, 10},
+    {9, 11},
+    {9, 11},
+    {8, 12},
+    {8, 12},
+    {8, 12},
+    {7, 13},
+    {7, 13},
+    {6, 14},
+    {6, 14},
+    {6, 14},
+    {5, 15},
+    {5, 15},
+    {4, 16},
+    {4, 16},
+    {4, 16},
+    {3, 17},
+    {3, 17},
+    {2, 18},
+    {2, 18},
+    {2, 18},
+    {1, 19},
+    {1, 19},
+    {0, 20},
+    {0, 20},
+}
+
 type ChristmasSlide struct {
-    PtoDate  time.Time
-    XmasDate time.Time
+    XmasDate     time.Time
+    RedrawTicker *time.Ticker
 }
 
 func NewChristmasSlide() *ChristmasSlide {
@@ -20,7 +49,6 @@ func NewChristmasSlide() *ChristmasSlide {
 
 func (this *ChristmasSlide) Initialize() {
     t := time.Now()
-    this.PtoDate = time.Date(t.Year(), time.December, 21, 0, 0, 0, 0, time.Local)
     this.XmasDate = time.Date(t.Year(), time.December, 25, 0, 0, 0, 0, time.Local)
 }
 
@@ -29,7 +57,7 @@ func (this *ChristmasSlide) Terminate() {
 }
 
 func (this *ChristmasSlide) StartDraw(d Display) {
-    DrawOnce(d, this.Draw)
+    this.RedrawTicker = DrawEverySecond(d, this.Draw)
 }
 
 func (this *ChristmasSlide) StopDraw() {
@@ -41,18 +69,53 @@ func (this *ChristmasSlide) IsEnabled() bool {
 }
 
 func (this *ChristmasSlide) Draw(img *image.RGBA) {
-    r := color.RGBA{255, 0, 0, 255}
-    g := color.RGBA{0, 255, 0, 255}
+    red := color.RGBA{255, 0, 0, 255}
+    green := color.RGBA{0, 255, 0, 255}
+    darkgreen := color.RGBA{0, 128, 0, 255}
+    yellow := color.RGBA{255, 255, 0, 255}
+    brown := color.RGBA{255, 128, 0, 255}
+    aqua := color.RGBA{0, 255, 255, 255}
+    blue := color.RGBA{0, 0, 255, 255}
+    orange := color.RGBA{255, 220, 0, 255}
 
-    DrawEmptyBox(img, r, 23, 1, 18, 13)
-    WriteString(img, fmt.Sprintf("%d", this.DaysUntil(this.PtoDate)), r, ALIGN_CENTER, 32, 4)
-    WriteString(img, "DAYS UNTIL", g, ALIGN_CENTER, 32, 16)
-    WriteString(img, "VACATION", g, ALIGN_CENTER, 32, 24)
+    lights := []color.RGBA{
+        aqua, aqua,
+        red, red,
+        green, green,
+        blue, blue,
+        orange, orange,
+    }
 
-    DrawEmptyBox(img, r, 87, 1, 18, 13)
-    WriteString(img, fmt.Sprintf("%d", this.DaysUntil(this.XmasDate)), r, ALIGN_CENTER, 96, 4)
-    WriteString(img, "DAYS UNTIL", g, ALIGN_CENTER, 96, 16)
-    WriteString(img, "CHRISTMAS", g, ALIGN_CENTER, 96, 24)
+    treeOffsetX := 18
+    treeOffsetY := 2
+
+    // Draw the star
+    img.SetRGBA(treeOffsetX+10, treeOffsetY-1, yellow)
+    // Draw the tree body
+    for j, line := range TreeDef {
+        DrawHorizLine(img, darkgreen, treeOffsetX+line[0], treeOffsetX+line[1], treeOffsetY+j)
+    }
+    // Draw the stump
+    DrawBox(img, brown, treeOffsetX+9, treeOffsetY+25, 3, 5)
+
+    // Draw some sparkles
+    for _, c := range lights {
+        for {
+            x := rand.Intn(21)
+            y := rand.Intn(24) + 1
+            // Don't draw out of tree's bounds
+            if x > TreeDef[y][0] && x < TreeDef[y][1] {
+                img.SetRGBA(treeOffsetX+x, treeOffsetY+y, c)
+                break
+            }
+        }
+    }
+
+    countdownOffsetX := 82
+    DrawEmptyBox(img, red, countdownOffsetX - 9, 1, 18, 13)
+    WriteString(img, fmt.Sprintf("%d", this.DaysUntil(this.XmasDate)), red, ALIGN_CENTER, countdownOffsetX, 4)
+    WriteString(img, "DAYS UNTIL", green, ALIGN_CENTER, countdownOffsetX, 16)
+    WriteString(img, "CHRISTMAS", green, ALIGN_CENTER, countdownOffsetX, 24)
 }
 
 func (this *ChristmasSlide) DaysUntil(d time.Time) int {
